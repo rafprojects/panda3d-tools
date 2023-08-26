ecef20247f6e2d7758113345fdf81999021af25e
Raf <rt3426@protonmail.com>
2023-08-26 19:23:47 -0400

    moving some things around

--- scripts/spritegen.py
+++ scripts/spritegen.py
@@ -1,39 +1,12 @@
-from PIL import Image
+from PIL import ImageDraw
 import random
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-from opensimplex import OpenSimplex
+from tilehelpers import palettesD, save_img, get_simplex_color, get_other_pixel_colors
+from tilegenerator import TileCanvasGenerator
 
-
-palettesD = {
-    'regular_grass': [(i, random.randint(100, 255), i) for i in range(50, 100)],
-    'dirt': [(i, int(i / 1.5), int(i / 3)) for i in range(85, 160)],
-    'dry_grass': [(i, i, int(i / 2)) for i in range(160, 230)],
-    'green_grass': [(i, random.randint(100, 255), i) for i in range(50, 100)],
-    'lava': [(random.randint(200, 255), i, int(i / 4)) for i in range(10, 100)],
-    'ocean': [(i, i, random.randint(100, 255)) for i in range(50, 100)],
-    'sand': [(i, i, int(i / 1.2)) for i in range(200, 255)],
-    'rock': [(i, i, i) for i in range(50, 120)],
-    'river': [(i, i, random.randint(150, 255)) for i in range(70, 100)],
-    'swamp': [(i, random.randint(100, 150), i) for i in range(50, 80)],
-    'pond': [(i, i, random.randint(120, 200)) for i in range(60, 90)]
-}
-tile_settings = {
-    'regular_grass': {
-        'variations': ['dirt', 'dry_grass']
-    },
-    'dirt': {
-        'variations': ['dry_grass', 'rock']
-    },
-    'dry_grass': {
-        'variations': ['sand', 'rock']
-    },
-    'green_grass': {
-        'variations': ['dirt', 'regular_grass']
-    }
-}
 
 def generate_rand_template(x, y, show=False):
     """Generate numpy template 'tile' of on/off values"""
@@ -58,137 +31,124 @@ def generate_rand_template(x, y, show=False):
         return template
 
 
-class TileGenerator():
-    def __init__(self, img_x, img_y, simplex_scale, palettesD, tile_settings, 
-                 palette_var_thresh_A=0.3, palette_var_thresh_B=0.6, save_location=None, simplex=True):
-        self.save_location = save_location
-        self.tile_settings = tile_settings
-        self.img_x = img_x
-        self.img_y = img_y
-        self.simplex_enabled = simplex
-        self.simplex_scale = simplex_scale
-        self.img = None  # the output image
-        self.simplex_gen = OpenSimplex(seed=random.randint(0, 1000))
-        self.color_range = None  # store range selected from palettesD
-        self.palettesD = palettesD
-        self.palette_var_thresh_A = palette_var_thresh_A 
-        self.palette_var_thresh_B = palette_var_thresh_B
+def gen_simple_tile(img_obj, tile_type, variance=False, simplex_enabled=False, simplex_scale=None, palette_var_thresh_A=None, palette_var_thresh_B=None, save=False, save_location=None):
+    # size = (img_x, img_y)
+    color_range = palettesD[tile_type]
+    # img = Image.new('RGB', size)
 
-    def gen_simple_tile(self, type, variance=False):
-        # Define your size and color range
-        size = (self.img_x, self.img_y)
-        self.color_range = palettesD[type]
-        # Create a new image
-        self.img = Image.new('RGB', size)
-        # Get the pixels of the image
-        pixels = self.img.load()
-        # Pixel & color inits
-        prev_pixel = [0, 0]
-        prev_i = 0
-        up_pixel = [0, 0]
-        up_j = 0
-        down_pixel = [0, 0]
-        down_j = 0
-        pixel_count = 0
-        prev_color, up_color, down_color = None, None, None
-        # Iterate over each pixel
-        for i in range(self.img.size[0]):    # for every column
-            if i > 0:
-                prev_i = i - 1
-            for j in range(self.img.size[1]):    # for every row
-                if i > 0 and j > 0:
-                    if j != 0:
-                        up_j = j - 1
-                    if j < self.img_y - 1:
-                        down_j = j + 1
-                    prev_pixel = (prev_i, j)
-                    up_pixel = (i, up_j)
-                    down_pixel = (i, down_j)
-              
-                    prev_color, up_color, down_color = self.get_other_pixel_colors(
-                        self.img,
-                        prev_pixel,
-                        up_pixel,
-                        down_pixel
-                    )
-          
-                # Choose a random color from the color range
-                if self.simplex_enabled:
-                    color = self.get_simplex_color(
-                        type=type,
-                        i=i,
-                        j=j,
-                        scale=self.simplex_scale,
-                        color_range=self.color_range,
-                        variance=variance
-                    )
-                else:
-                    color = random.choice(self.color_range)
-                print(f"CURRENT: {[i, j]} | {color}")
-                print(f'LEFT: {prev_pixel} | {prev_color}')
-                print(f'UP: {up_pixel} | {up_color}')
-                print(f'DOWN: {down_pixel} | {down_color}')
-                print()
-                
-                # Set the pixel's color
-                pixels[i, j] = color
-                pixel_count += 1
-        print(pixel_count)
-        # Save the image
-        if self.save_location:
-            self.img.save(f'{self.save_location}/{type}2.png')
-        else:
-            self.img.save(f'{type}.png')
-        
-    def get_other_pixel_colors(self, img, prev, up, down):
-        '''Grab colors from surrounding pixels'''
-        return img.getpixel(prev), img.getpixel(up), img.getpixel(down)
+    # Get the pixels of the image
+    pixels = img_obj.load()
+    # Pixel & color inits
+    prev_pixel = [0, 0]
+    prev_i = 0
+    up_pixel = [0, 0]
+    up_j = 0
+    down_pixel = [0, 0]
+    down_j = 0
+    pixel_count = 0
+    prev_color, up_color, down_color = None, None, None
+    # Iterate over each pixel
+    for i in range(img_obj.size[0]):    # for every column
+        if i > 0:
+            prev_i = i - 1
+        for j in range(img_obj.size[1]):    # for every row
+            if i > 0 and j > 0:
+                if j != 0:
+                    up_j = j - 1
+                if j < img_obj.size[1] - 1:
+                    down_j = j + 1
+                prev_pixel = (prev_i, j)
+                up_pixel = (i, up_j)
+                down_pixel = (i, down_j)
 
-    def get_simplex_color(self, type, i, j, scale, color_range, i_inc=0, j_inc=0, variance=False):
-        """scale determines "zoom level", smaller = larger patterns"""
-        # Gen noise val btwn -1 to 1
-        noise_value = self.simplex_gen.noise2((i / scale) + i_inc, (j / scale) + j_inc)
-        color_idx = 0
-        if variance:
-            # palette = color_range
-            palette, scaled_noise_val = self.palette_by_noise_val(
-                type=type,
-                noise_val=noise_value,
-                palettesD=self.palettesD,
-                threshold_A=self.palette_var_thresh_A,
-                threshold_B=self.palette_var_thresh_B
-            )
-            color_idx = int(scaled_noise_val * len(palette))
-        else:
-            # scale noise val to range of palette
-            color_idx = int((noise_value + 1) / 2 * len(color_range))
-        # return color from palette
-        return color_range[color_idx]
+                prev_color, up_color, down_color = get_other_pixel_colors(
+                    img_obj,
+                    prev_pixel,
+                    up_pixel,
+                    down_pixel
+                )
+            # Choose a random color from the color range
+            if simplex_enabled:
+                color = get_simplex_color(
+                    type=tile_type,
+                    i=i,
+                    j=j,
+                    scale=simplex_scale,
+                    color_range=color_range,
+                    variance=variance,
+                    palette_var_thresh_A=palette_var_thresh_A,
+                    palette_var_thresh_B=palette_var_thresh_B
+                )
+            else:
+                color = random.choice(color_range)
+            # print(f"CURRENT: {[i, j]} | {color}")
+            # print(f'LEFT: {prev_pixel} | {prev_color}')
+            # print(f'UP: {up_pixel} | {up_color}')
+            # print(f'DOWN: {down_pixel} | {down_color}')
+            print()
+            # Set the pixel's color
+            pixels[i, j] = color
+            pixel_count += 1
+    print(pixel_count)
 
-    def palette_by_noise_val(self, type, noise_val, palettesD, threshold_A, threshold_B):
-        scaled_noise_val = (noise_val + 1) / 2
-        print(scaled_noise_val)
-        type_variationsL = self.tile_settings[type]['variations']
-        print(type_variationsL)
-        if scaled_noise_val < threshold_A:
-            print(type_variationsL[0])
-            return palettesD[type_variationsL[0]], scaled_noise_val
-        elif scaled_noise_val < threshold_B:
-            print(type_variationsL[1])
-            return palettesD[type_variationsL[1]], scaled_noise_val
-        else:
-            return palettesD[type], scaled_noise_val
-  
-tilegen = TileGenerator(
-    img_x=64,
-    img_y=64,
-    simplex=True,
-    simplex_scale=6,
-    palettesD=palettesD,
-    palette_var_thresh_A=0.4,
-    palette_var_thresh_B=0.6,
-    tile_settings=tile_settings
-)
-tilegen.gen_simple_tile(type='dirt', variance=True)
+    if save:
+        save_img(img=img_obj, save_location=save_location, img_name=tile_type)
+    else:
+        return img_obj
 
+
+def generate_brick_pattern(img_obj, size, brick_count_x, brick_count_y, brick_color, mortar_color, mortar_size=2, save=False, save_location=None):
+    draw = ImageDraw.Draw(img_obj)
+    # Calculate the size of bricks and mortar
+    brick_width = (size[0] - (brick_count_x - 1) * mortar_size) // brick_count_x
+    brick_height = (size[1] - (brick_count_y - 1) * mortar_size) // brick_count_y
+    # Full and half brick width
+    full_brick = (brick_width, brick_height)
+    half_brick = (brick_width // 2, brick_height)
+    # Draw each row
+    for j in range(brick_count_y):
+        y_top = j * (brick_height + mortar_size)
+        y_bottom = y_top + brick_height
+        # Determine the pattern for this row
+        pattern = [full_brick] * brick_count_x if j % 2 == 0 else [half_brick] + [full_brick] * (brick_count_x - 1)
+        x_left = 0
+        for brick in pattern:
+            x_right = x_left + brick[0]
+            # If this brick goes beyond the edge, trim it
+            if x_right > size[0]:
+                x_right = size[0]
+            # Draw the brick
+            draw.rectangle([x_left, y_top, x_right, y_bottom], fill=brick_color)
+            # Next brick position
+            x_left = x_right + mortar_size
+    # Draw the bottom row if there is space left
+    if y_bottom < size[1]:
+        draw.rectangle([0, y_bottom, size[0], size[1]], fill=mortar_color)
+    if save:
+        save_img(img=img_obj, save_location=save_location, img_name='brick_tessel2')
+    else:
+        return img_obj
+
+
+def gen_grass_tile(draw_obj, direction='straight'):
+    base_color_range = palettesD['regular_grass']
+
+
+
+# def gen_rock_tile(base_shape):
+#     tile = np.copy(base_shape)
+#     for i in range(tile.shape[0]):
+#         for j in range(tile.shape[1]):
+#             # gen rand variations
+#             if tile[i][j] == 1 and random.random() < 0.1:  # 10% chance to become a 0
+#                 tile[i][j] = 0
+#     return tile
+
+# TESTING
+
+tg = TileCanvasGenerator(64, 64)
+tile = tg.make_empty_tile()
+
+gen_simple_tile(tile, tile_type='ocean', variance=True, simplex_enabled=True, simplex_scale=2, palette_var_thresh_A=4, palette_var_thresh_B=6, save=True, save_location='.')
+# tilegen.generate_brick_pattern(size=(128, 128), brick_count_x=12, brick_count_y=20, brick_color=(150, 75, 50), mortar_color=(200, 200, 200))
 # generate_rand_template(64, 64, True)
---
+++ scripts/tilehelpers.py
@@ -0,0 +1,103 @@
+import random
+
+from opensimplex import OpenSimplex
+
+palettesD = {
+    'regular_grass': [(i, random.randint(100, 255), i) for i in range(50, 100)],
+    'dirt': [(i, int(i / 1.5), int(i / 3)) for i in range(85, 160)],
+    'dry_grass': [(i, i, int(i / 2)) for i in range(160, 230)],
+    'green_grass': [(i, random.randint(100, 255), i) for i in range(50, 100)],
+    'lava': [(random.randint(200, 255), i, int(i / 4)) for i in range(10, 100)],
+    'ocean': [(i, i, random.randint(100, 255)) for i in range(50, 100)],
+    'sand': [(i, i, int(i / 1.2)) for i in range(200, 255)],
+    'rock': [(i, i, i) for i in range(50, 120)],
+    'river': [(i, i, random.randint(150, 255)) for i in range(70, 100)],
+    'swamp': [(i, random.randint(100, 150), i) for i in range(50, 80)],
+    'pond': [(i, i, random.randint(120, 200)) for i in range(60, 90)]
+}
+tile_settings = {
+    'regular_grass': {
+        'variations': ['dirt', 'dry_grass']
+    },
+    'dirt': {
+        'variations': ['dry_grass', 'rock']
+    },
+    'dry_grass': {
+        'variations': ['sand', 'rock']
+    },
+    'green_grass': {
+        'variations': ['dirt', 'regular_grass']
+    }
+}
+
+base_rock_shape = [
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1]
+]
+
+
+def save_img(img, save_location, img_name):
+    if save_location:
+        img.save(f'{save_location}/{img_name}2-4-6.png')
+    else:
+        img.save(f'{img_name}.png')
+
+
+def get_simplex_color(type, i, j, scale, color_range, i_inc=0, j_inc=0, variance=False, palette_var_thresh_A=None, palette_var_thresh_B=None):
+        """scale determines "zoom level", smaller = larger patterns"""
+        # Gen noise val btwn -1 to 1
+        simplex_gen = OpenSimplex(seed=random.randint(0, 1000))
+        noise_value = simplex_gen.noise2((i / scale) + i_inc, (j / scale) + j_inc)
+        color_idx = 0
+        if variance:
+            # palette = color_range
+            palette, scaled_noise_val = palette_by_noise_val(
+                type=type,
+                noise_val=noise_value,
+                palettesD=palettesD,
+                threshold_A=palette_var_thresh_A,
+                threshold_B=palette_var_thresh_B
+            )
+            color_idx = int(scaled_noise_val * len(palette))
+        else:
+            # scale noise val to range of palette
+            color_idx = int((noise_value + 1) / 2 * len(color_range))
+        # return color from palette
+        return color_range[color_idx]
+    
+    
+def get_other_pixel_colors(img, prev, up, down):
+    '''Grab colors from surrounding pixels'''
+    return img.getpixel(prev), img.getpixel(up), img.getpixel(down)
+
+
+def palette_by_noise_val(type, noise_val, palettesD, threshold_A, threshold_B):
+    scaled_noise_val = (noise_val + 1) / 2
+    print(scaled_noise_val)
+    variations = tile_settings.get(type, {})
+    if variations:
+        type_variationsL = tile_settings[type]['variations']
+        print(type_variationsL)
+        if scaled_noise_val < threshold_A:
+            print(type_variationsL[0])
+            return palettesD[type_variationsL[0]], scaled_noise_val
+        elif scaled_noise_val < threshold_B:
+            print(type_variationsL[1])
+            return palettesD[type_variationsL[1]], scaled_noise_val
+    else:
+        return palettesD[type], scaled_noise_val
+
+
+def draw_grass_blade(draw, x, y, direction, color, length_factor=1.0):
+    # draw = ImageDraw object
+    length = random.randint(5, 15) * length_factor
+    if direction == 'left':
+        end_x = x - random.randint(0, 5)
+    elif direction == 'right':
+        end_x = x + random.randint(0, 5)
+    else:
+        end_x = x
+    end_y = y - length
+    draw.line([(x, y), (end_x, end_y)], fill=color, width=1)
